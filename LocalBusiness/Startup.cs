21630d86098a6dad0c08c6b5d1191c07e0d6ee9c
blacchoy@gmail.com <blacchoy@gmail.com>
2020-08-21 15:21:56 -0700

    add swagger to csproj

--- LocalBusiness/Startup.cs
+++ LocalBusiness/Startup.cs
@@ -21,6 +21,11 @@ namespace LocalBusiness
             services.AddDbContext<LocalBusinessContext>(opt =>
                 opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+                services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -33,6 +38,16 @@ namespace LocalBusiness
             {
                 app.UseHsts();
             }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             
             app.UseMvc();
         }
